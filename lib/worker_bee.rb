7f609c147350b679c675753c11a99036f6a64a8f
Brandon Caplan <bcaplan@onehub.com>
2009-04-23 19:35:23 -0700

    Tests added for basic method requirements.

--- lib/worker_bee.rb
+++ lib/worker_bee.rb
@@ -1,3 +1,20 @@
 class WorkerBee
   VERSION = '1.0.0'
+  
+  def initialize
+    
+  end
+  
+  def self.recipe
+    
+  end
+  
+  def self.run
+    
+  end
+  
+  def task
+    
+  end
+  
 end
--- test/test_worker_bee.rb
+++ test/test_worker_bee.rb
@@ -2,7 +2,34 @@ require "test/unit"
 require "worker_bee"
 
 class TestWorkerBee < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def setup
+     wb = WorkerBee.new
   end
+  
+  def test_recipe_takes_a_block
+    expected = 'hello'
+    actual = WorkerBee.recipe do
+      'hello'
+    end    
+    
+    assert_equal expected, actual
+  end
+  
+  def test_task_takes_a_block    
+    expected = 'hello'
+    actual = wb.task do
+      'hello'
+    end
+    
+    assert_equal expected, actual
+  end
+  
+  def test_task_takes_many_arguments
+    assert_nothing_raised(ArgumentError) do
+      WorkerBee.new.task :one, :two, :three, :four do
+        'hello'
+      end
+    end
+  end
+  
 end
